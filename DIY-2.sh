e74c6ddf9a47b5183a306ecd5f0ab19d7f686ed7
悟★空 <154215968+WukongMaster@users.noreply.github.com>
2024-10-18 13:29:57 +0800

    Add files via upload

---
+++ DIY-2.sh
@@ -0,0 +1,10 @@
+# 此脚本用处是：定制个性化参数
+#============================================================================================
+
+
+# 1-设置管理地址
+sed -i 's/192.168.1.1/192.168.2.100/g' package/base-files/files/bin/config_generate
+
+# 2-设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
