63747d45d98013ff151ad758f4f2e14695a57e46
悟★空 <154215968+WukongMaster@users.noreply.github.com>
2024-10-20 13:38:07 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,8 +1,5 @@
 # 此脚本用处是：定制个性化参数
-#=================================================
+#=====================================================================
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-
-
