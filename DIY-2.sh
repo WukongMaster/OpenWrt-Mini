e4f934f45a04df3d44534a1070e2c1f59d82c549
悟★空 <154215968+WukongMaster@users.noreply.github.com>
2024-10-20 13:36:19 +0800

    Add files via upload

---
+++ DIY-2.sh
@@ -0,0 +1,8 @@
+# 此脚本用处是：定制个性化参数
+#=================================================
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+
+
