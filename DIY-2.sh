3cf3e6a42a4a108e53f7b6736052a31a96fc5761
悟★空 <154215968+WukongMaster@users.noreply.github.com>
2024-10-20 20:05:30 +0800

    Delete DIY-2.sh

--- DIY-2.sh
+++
@@ -1,9 +0,0 @@
-# 此脚本用处是：定制个性化参数
-#=====================================================================
-
-# 1-替换新版 golang
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-# 2-设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
