f4e66951c2f0d69f1020434161fe8727d8a9ce58
悟★空 <154215968+WukongMaster@users.noreply.github.com>
2024-12-01 11:55:56 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -2,7 +2,7 @@
 # ============================================================================================
 
 # 1-修改管理地址
-sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 2-修改内核版本
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
