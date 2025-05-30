7798a044c5f65b57945259e218a4331e24ddafa9
home_pc <2016ice@gmail.com>
2025-05-31 03:14:04 +0800

    add passwall 2

--- DIY-1.sh
+++ DIY-1.sh
@@ -14,5 +14,5 @@ git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 5-添加 PassWall 插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
